878bc308641cfc02060bfc490df360eba38e87dc
Unknown <dani_mrx93@hotmail.com>
2020-02-27 02:29:45 -0300

    Add difficulty on trees

--- YeOldLumber/Scripts/GameManager.gd
+++ YeOldLumber/Scripts/GameManager.gd
@@ -3,11 +3,4 @@ extends Node
 var wood_ui_pos : Vector2
 var wood_count : int = 0
 var gold_count : int = 0
-
-signal update_currency
-
-func sell_wooden():
-	gold_count += wood_count
-	wood_count = 0
-	emit_signal('update_currency')
-	pass
+var difficulty_level : int = 1
