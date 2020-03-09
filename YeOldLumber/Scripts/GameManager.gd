7b226cfdfeb8b8468a11e2f6e4ea708c9b953f46
Daniel <danielnatarelli@hotmail.com>
2020-03-09 20:38:01 -0300

    Now woods are spawn be tree

--- YeOldLumber/Scripts/GameManager.gd
+++ YeOldLumber/Scripts/GameManager.gd
@@ -4,3 +4,16 @@ var wood_ui_pos : Vector2
 var wood_count : int = 0
 var gold_count : int = 0
 var difficulty_level : int = 1
+var game_camera : Camera
+var middle_panel : Node = null
+
+signal on_wood_change(wood_count)
+
+func set_rect_on_screen(rect : Control, pos : Vector3):
+	rect.rect_global_position = game_camera.unproject_position(pos) - Vector2(50,0)
+	pass
+
+func add_wood(val):
+	wood_count += val
+	emit_signal('on_wood_change', wood_count)
+	pass
